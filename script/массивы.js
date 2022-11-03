186db1d70011886681a8b75ffe1f730bd9cd0ab7
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2022-11-03 20:19:28 +0300

    feat: обавил новые данные

---
+++ script/массивы.js
@@ -0,0 +1,36 @@
+//Создание пустого массива
+const emptyArr = [];
+// Более сложный метод
+const arr = new Array(56,"string");
+// Более простой метод
+const easyArr = [44,"string",true, 0, -55];
+// Поиск элемента по индексу
+easyArr[2];
+// Присвоить другой элемент списку
+easyArr[2] = false;
+// Длинна массива
+easyArr.length;
+
+
+
+// Многомерные массивы
+let arr1 = ["str", 34, -4, null];
+let arr2 = [4000000, Object, undefined, 'str'];
+let arr3 = [22.3, Infinity, 4434, -5];
+
+let grupArr = [arr1, arr2, arr3];
+
+// Показать весь список
+console.log(grupArr);
+// Узнать элемент по индексу массива
+grupArr[1][3];
+
+// Другой вариант записи
+let bigArr = [
+  [33, 'string', null, 44.8], ['string', -398, -Infinity, undefined], [Object, 43, 'string', false]
+];
+
+// Замена значения элемента в списке
+bigArr[2][2] = 'newString';
+// Показать весь список 
+console.log(bigArr);
---
+++ script/математические-действия.js
@@ -0,0 +1,34 @@
+let num1 = 404;
+let num2 = 2;
+
+console.log(num1 + num2); // сложение
+console.log(num1 - num2); // вычитание
+console.log(num1 * num2); // умножение
+console.log(num1 / num2); // деление
+console.log(num1 % num2); // остаток от деления 
+
+let num3 = 10;
+num3 = num3 + 90; // num3 = 100
+// сокращенная запись 
+let num4 = 10;
+num4 += 90 // num4 = 100
+// добавить еденицу
+num4 ++; //num4 = 101;
+
+// Сложение строк
+let str_1 = 'один';
+let str_2 = 'два';
+console.log(str_1 + str_2); // одиндва
+
+let str_3 = 88;
+let str_4 = '25';
+console.log(str_3 + str_4); // 8825
+// преобразование строки в число
+str_4 = Number('25')
+console.log(str_3 + str_4); // 113
+
+// Встроенный объект Math.
+Math.max(34, 56, -2, 100); // метод .max определит наибольшее число
+Math.min(34, 56, -2, 100); // метод .min определит наименьшее число
+
+
