7b69fbfee40abf8f793f67ee823dff24db7703db
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2022-11-23 21:25:51 +0300

    refactor: внес изменения

--- script/события/события-мыши.js
+++ script/события/события-мыши.js
@@ -1,2 +1,23 @@
 // 🧰 Объект event — мышь 🧰
 
+// https://code.s3.yandex.net/web-developer/learning-materials/mouseover-ru.mp4
+
+// событя мыши для Плейлиста 
+
+const coverHeading = document.querySelector('.cover__heading');
+const playListTitles = [
+  'Сборник хитов',
+  'ЯЯЯЯЯЯЯЯЯЯЗЬ',
+  'Вышел кролик на крыльцо, почесать свое яйцо',
+  'А еще раз',
+  'Полный улет',
+];
+
+function getRandomElement(arr) {
+  const randomId = Math.floor(Math.random() * arr.length);
+  return arr[randomId];
+}
+
+coverHeading.addEventListener('dblclick', () => {
+  coverHeading.textContent = getRandomElement(playListTitles);
+});
