6b48482fa64d5934371a1d77994a496bfb5c00d5
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2022-12-13 22:52:42 +0300

    script: изменил функции

--- script/oop/oop-interface.js
+++ script/oop/oop-interface.js
@@ -1,20 +1,65 @@
 // ООП в интерфейсах
 
+const items = [
+  {
+    image: 'https://code.s3.yandex.net/web-code/oop/card_detail.jpg',
+    title: 'BIOLOID',
+    description: 'Это популярная серия программируемых робототехнических конструкторов компании Robotis. Серия представлена разнообразными универсальными наборами, которые подойдут как начинающим робототехникам, так и специалистам, работающим над решением актуальных робототехнических задач. Также в наборе есть пульт для управления роботом и набор пластиковых элементов для придания уникального вида собранным моделям.',
+    price: '82 000'
+  },
+  {
+    image: 'https://code.s3.yandex.net/web-code/oop/card_detail.jpg',
+    title: 'BIOLOID Premium kit',
+    description: 'BIOLOID Premium kit – набор для создания различных шагающих роботов на основе моторов Dynamixel и контроллера СМ-530, для образования, игр и соревнований.',
+    price: '126 000'
+  },
+  {
+    image: 'https://code.s3.yandex.net/web-code/oop/card_detail2.png',
+    title: 'Airwheel',
+    description: 'Модель позволяет использовать при движении и педали и электрическую тягу, а также их сочетание. Съемный аккумулятор легко заменяется во время поездки, позволяя тем самым существенно увеличивать ее продолжительность.',
+    price: '145 000'
+  },
+];
+
 class Card {
-  constructor(title, description, price, image) {
-    this._title = title;
-    this._description =  description;
-    this._price =  price;
-    this._image =  image;
+  constructor(data, templateSelector) {
+    this._title = data.title;
+    this._description = data.description;
+    this._price = data.price;
+    this._image = data.image;
+    this._templateSelector = templateSelector;
   }
-  
+
   _getTemplate() {
-    const cardElem = document.querySelector('.horizontal-card').content.querySelector('.card').cloneNode(true);
-    return cardElem;
+    const cardElement = document
+      .querySelector(this._templateSelector)
+      .content
+      .querySelector('.card')
+      .cloneNode(true);
+
+    return cardElement;
+  }
+
+  generateCard() {
+    this._element = this._getTemplate();
+
+    this._element.querySelector('.card__image').style.backgroundImage = `url(${this._image})`;
+    this._element.querySelector('.card__title').textContent = this._title;
+    this._element.querySelector('.card__info').textContent = this._description;
+    this._element.querySelector('.card__price-property').textContent = this._price;
+
+    return this._element;
   }
 }
-const test1 = new Card;
-console.log(test1._getTemplate())
+
+items.forEach((item) => {
+  const card = new Card(item, '.horizontal-card');
+  const cardElement = card.generateCard();
+
+  // Добавляем в DOM
+  document.querySelector('.card-list__items').append(cardElement);
+});
+
 
 // ✅ для мессенджера
 
@@ -30,34 +75,46 @@ const messageList = [
 ];
 
 class Message {
-	constructor(text, image) {
-		this._text = text;
-		this._image = image;
+	constructor(data, selector) {
+		this._text = data.text;
+		this._image = data.image;
+		this._selector = selector;
 	}
-  	
-  _getElement() {
+
+	_getElement() {
     const messageElement = document
-      .querySelector('.message-template')
+      .querySelector(this._selector)
       .content
       .querySelector('.message')
       .cloneNode(true);
-          
+
     return messageElement;
-  }
-  
+	}
+
   generate() {
     this._element = this._getElement();
-    
-    this._element.querySelector('.message__avatar').src = this._image;
-    this._element.querySelector('.message__paragraph').textContent = this._text;
-    
-    return this._element;
+  	this._element.querySelector('.message__avatar').src = this._image;
+  	this._element.querySelector('.message__paragraph').textContent = this._text;
+
+    this._setEventListeners();
+
+  	return this._element;
+  }
+
+  _setEventListeners() {
+    this._element.querySelector('.message__text').addEventListener('click', () => {
+      this._handleClick();
+    });
+  }
+
+  _handleClick() {
+    this._element.querySelector('.message__text').classList.toggle('message__text_is-active');
   }
 }
-const body = document.querySelector('#body')
+const page = document.querySelector('.page')
 messageList.forEach((item) => {
-	const message = new Message(item.text, item.image);
+	const message = new Message(item, '.message-template_type_default');
 	const messageElement = message.generate();
-  
-  body.append(messageElement);
+
+	page.append(messageElement);
 });
