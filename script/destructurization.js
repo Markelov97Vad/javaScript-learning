207e70a80a65aa73a6e930f74b872ed3a849b1f8
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2022-12-23 21:25:03 +0300

    feat: Destructurization.js

--- script/destructurization.js
+++ script/destructurization.js
@@ -1,4 +1,5 @@
 // Деструктуризация
+// Деструктуризация — это способ записи значений в переменные. Используя её, можно заметно сократить код. 
 
 function calcValue1 (a,b) {
   return [
@@ -42,3 +43,100 @@ function logPers ({name: fName, ageH}) {
 }
 
 logPers(human1);
+
+// ✅ Объект
+
+const { A: type, B: nameV } = { A: 'кресло', B: 'Василий' };
+console.log(type, nameV); // кресло Василий
+
+// ✅ Массив
+
+const [type1, nameZ] = ['кресло', 'Василий'];
+console.log(type1, nameZ); // кресло Василий
+
+// ✅ Функция
+
+const chair = { type: 'кресло', name: 'Василий' };
+
+const whereToSit = ({ type, name }) => {
+return `Садитесь в ${type} «${name}»`;
+};
+
+console.log(whereToSit(chair)); // Садитесь в кресло «Василий»
+
+// ✅ значения по умолчанию:
+
+const { nameNew= 'Василий' } = {};
+const [typeNew = 'кресло'] = [];
+
+console.log(typeNew, nameNew); // кресло Василий
+
+// ✅
+
+const greekGods = { love: 'Афродита', war: 'Арес', trade: 'Гермес' };
+
+/* перечисляем свойства, которые нужно достать, в фигурных скобках */
+const { love, war, trade } = greekGods; // имена переменных совпадают с ключами объекта
+
+console.log(love, trade); // Афродита Арес Гермес
+
+// ✅
+
+const precipitation = ['дождь', 'морось', 'роса'];
+const [rain, drizzle, dew] = precipitation;
+
+console.log(rain, drizzle, dew); // дождь морось роса 
+
+const userNameInfo = {
+  name:'Семен',
+  work: 'разработчик'
+}
+
+const paymentInfo = {
+  method: 'card',
+  amount: 1000,
+  currency: 'rub'  
+};
+
+const printUserParams = ({ name: nameInUserN , work}, { amount, ...payment }) => {
+  console.log( nameInUserN, work);
+  console.log(amount, payment);
+};
+console.clear()
+printUserParams(userNameInfo, paymentInfo);
+
+// Класс с использование деструктуризации ✅
+
+const textZet = 'Цель оправдывает средства';
+const picMyLife = 'https://budapest';
+const loveMoment = 'Music'
+class Destruct {
+  constructor({textZet, picMyLife, loveMoment}){
+    this._textZet = textZet;
+    this._picMyLife = picMyLife;
+    this._loveMoment = loveMoment;
+  }
+}
+
+// Если приходит объект без параметра, то значение будет по умолчанию ✅
+
+function consUserInf ({name = 'Монгол', dogName ="Гошан"}) {
+  //const {name = 'Монгол', dogName ="Гошан"} = user;
+  console.log(`${name}, ${dogName}`);
+}
+consUserInf({name:'Иришка', dogName:'Геркулес'});
+consUserInf({dogName:'Тузик'});
+
+// ✅
+
+function getFirstThreeUsers(users) {
+  const value = [firstOf = 'Дэвид', secondOf = 'Леонард', thirdOf = 'Стивен'] = users;
+  console.log(firstOf, secondOf, thirdOf);
+}
+
+getFirstThreeUsers(['пахом', 'Венцислав']); // пахом Венцислав Стивен
+
+   
+
+
+   
--- script/object/test.js
+++ script/object/test.js
@@ -2,10 +2,58 @@ const mySity = {
   contry:'New York'
 };
 
-mySity.size = 'big';
-mySity.contry = null
-delete mySity.contry
-console.log(mySity);
+// mySity.size = 'big';
+// mySity.contry = null
+// delete mySity.contry
+// console.log(mySity);
 
-console.log(window)
-console.log(mySity.contry = {});
+// console.log(window)
+// console.log(mySity.contry = {});
+
+
+// function shortcut (string) {
+//   if(string === "a" || "e" || "i" || "o" || "u") {
+//     console.log('выполнено')
+//   }
+// }
+
+// console.log(shortcut('method'))
+
+// function shortcut (string) {
+//   if(string === "a" || "e" || "i" || "o" || "u") {
+//     // let a = string.split('').filter((item) => {
+//     //   let b = ["a", "e", "i", "o", "u"]
+//     //   item.includes(b)
+//     // });
+//     let v = []
+//     let a = string.split('');
+//     console.log(a)
+
+//     for (let i = 0 ; i < a.length; i++) {
+//       if(a.includes('a'))
+//     }
+
+//     // const b =  a.forEach((item) => {
+//     //   // return !(item === "a" || "e" || "i" || "o" || "u");
+//     //   return item.indexOf("a") && item.indexOf("e") && item.indexOf("i") && item.indexOf("o") && item.indexOf("u");
+//     // })
+//     // // indexOf('Ш')
+//     // // arr.splice(1, 1);
+//     // console.log(b)
+//   }
+// }
+
+function shortcut(string){
+  let vowels = 'aeiou';
+  
+  let result = [];
+  
+    for( let i = 0; i < string.length; i++){
+    if(!vowels.includes(string[i])){
+      result.push(string[i]);
+    }
+  }
+    return result.join('');
+  };
+
+console.log(shortcut('method'));
